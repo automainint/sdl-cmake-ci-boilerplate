bdb2acc7fe1797de69df02fc5ae6d0d4e6c93ff1
Mitya Selivanov <0x7fffff@guattari.ru>
2022-07-08 05:57:46 +0400

    Simple SDL example

---
+++ source/example/example.cpp
@@ -0,0 +1,68 @@
+/*  Copyright (c) 2022 Mitya Selivanov
+ */
+
+#include <iostream>
+#include <tuple>
+
+#include <SDL.h>
+#include <SDL_video.h>
+
+namespace cute::example {
+  using std::cout, std::tuple;
+
+  static constexpr int default_window_width  = 1024;
+  static constexpr int default_window_height = 768;
+
+  [[nodiscard]] auto default_window_rect() noexcept
+      -> tuple<int, int, int, int> {
+    int x      = SDL_WINDOWPOS_UNDEFINED;
+    int y      = SDL_WINDOWPOS_UNDEFINED;
+    int width  = default_window_width;
+    int height = default_window_height;
+
+    auto display_mode = SDL_DisplayMode {};
+    if (SDL_GetCurrentDisplayMode(0, &display_mode) < 0) {
+      cout << "SDL_GetCurrentDisplayMode failed: " << SDL_GetError()
+           << '\n';
+    } else {
+      x = display_mode.w / 2 - width / 2;
+      y = display_mode.h / 2 - height / 2;
+    }
+
+    return { x, y, width, height };
+  }
+
+  [[nodiscard]] auto run() -> int {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      cout << "SDL_Init failed: " << SDL_GetError() << '\n';
+      return 1;
+    }
+
+    auto [x, y, width, height] = default_window_rect();
+
+    auto window = SDL_CreateWindow(
+        "Cute Example", x, y, width, height,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    if (window == nullptr) {
+      cout << "SDL_CreateWindow failed: " << SDL_GetError() << '\n';
+      return 1;
+    }
+
+    for (bool done = false; !done;) {
+      auto event = SDL_Event {};
+      while (SDL_PollEvent(&event) == 1)
+        if (event.type == SDL_QUIT)
+          done = true;
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+  }
+}
+
+auto main(int argc, char **argv) -> int {
+  return cute::example::run();
+}
